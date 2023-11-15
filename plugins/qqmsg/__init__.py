f63b6f4e86248eadd0089c52b5f3ade3f6ad28c7
anjoyli <1589631311@qq.com>
2023-11-15 19:51:47 +0800

    update

--- plugins/qqmsg/__init__.py
+++ plugins/qqmsg/__init__.py
@@ -1,50 +1,51 @@
-from typing import Any, List, Dict, Tuple
+from urllib.parse import urlencode
 
-from pypushdeer import PushDeer
-
-from app.core.event import eventmanager, Event
-from app.log import logger
 from app.plugins import _PluginBase
+from app.core.event import eventmanager, Event
 from app.schemas.types import EventType, NotificationType
+from app.utils.http import RequestUtils
+from typing import Any, List, Dict, Tuple
+from app.log import logger
 
+import json
 
-class PushDeerMsg(_PluginBase):
+class QqMsg(_PluginBase):
     # 插件名称
-    plugin_name = "PushDeer消息通知"
+    plugin_name = "QQ消息通知"
     # 插件描述
-    plugin_desc = "支持使用PushDeer发送消息通知。"
+    plugin_desc = "支持使用QQ发送消息通知。"
     # 插件图标
-    plugin_icon = "pushdeer.png"
+    plugin_icon = "iyuu.png"
     # 主题色
-    plugin_color = "#FFFFFF"
+    plugin_color = "#00ffff"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "0.1"
     # 插件作者
-    plugin_author = "jxxghp"
+    plugin_author = "anjoyli"
     # 作者主页
-    author_url = "https://github.com/jxxghp"
+    author_url = "https://github.com/anjoyli"
     # 插件配置项ID前缀
-    plugin_config_prefix = "pushdeermsg_"
+    plugin_config_prefix = "qqmsg_"
     # 加载顺序
-    plugin_order = 26
+    plugin_order = 28
     # 可使用的用户级别
     auth_level = 1
 
     # 私有属性
     _enabled = False
-    _server = None
-    _apikey = None
+    _token = None
     _msgtypes = []
+    ## qq对应的api
+    _send_msg_url = "http://192.168.6.189:13579/LittlePaimon/api/message2admin"
 
     def init_plugin(self, config: dict = None):
         if config:
             self._enabled = config.get("enabled")
+            self._token = config.get("token")
             self._msgtypes = config.get("msgtypes") or []
-            self._server = config.get("server")
-            self._apikey = config.get("apikey")
 
     def get_state(self) -> bool:
-        return self._enabled and (True if self._server and self._apikey else False)
+        return self._enabled and (True if self._token else False)
 
     @staticmethod
     def get_command() -> List[Dict[str, Any]]:
@@ -95,33 +96,15 @@ class PushDeerMsg(_PluginBase):
                             {
                                 'component': 'VCol',
                                 'props': {
-                                    'cols': 12,
-                                    'md': 6
+                                    'cols': 12
                                 },
                                 'content': [
                                     {
                                         'component': 'VTextField',
                                         'props': {
-                                            'model': 'server',
-                                            'label': '服务器',
-                                            'placeholder': 'https://api2.pushdeer.com',
-                                        }
-                                    }
-                                ]
-                            },
-                            {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 6
-                                },
-                                'content': [
-                                    {
-                                        'component': 'VTextField',
-                                        'props': {
-                                            'model': 'apikey',
-                                            'label': '密钥',
-                                            'placeholder': 'PDUxxx',
+                                            'model': 'token',
+                                            'label': 'QQ令牌',
+                                            'placeholder': 'QQxxx',
                                         }
                                     }
                                 ]
@@ -155,9 +138,8 @@ class PushDeerMsg(_PluginBase):
             }
         ], {
             "enabled": False,
-            'msgtypes': [],
-            'server': 'https://api2.pushdeer.com',
-            'apikey': ''
+            'token': '',
+            'msgtypes': []
         }
 
     def get_page(self) -> List[dict]:
@@ -176,15 +158,17 @@ class PushDeerMsg(_PluginBase):
 
         msg_body = event.event_data
         # 渠道
-        channel = msg_body.get("channel")
-        if channel:
-            return
+        # channel = msg_body.get("channel")
+        # if channel:
+        #     return
         # 类型
         msg_type: NotificationType = msg_body.get("type")
         # 标题
         title = msg_body.get("title")
         # 文本
         text = msg_body.get("text")
+        # 图片
+        image = msg_body.get("image")
 
         if not title and not text:
             logger.warn("标题和内容不能同时为空")
@@ -196,16 +180,51 @@ class PushDeerMsg(_PluginBase):
             return
 
         try:
-            if not self._server or not self._apikey:
-                return False, "参数未配置"
-            pushdeer = PushDeer(server=self._server, pushkey=self._apikey)
-            res = pushdeer.send_markdown(title, desp=text)
-            if res:
-                logger.info(f"PushDeer消息发送成功")
-            else:
-                logger.warn(f"PushDeer消息发送失败！")
+            state, res = self.send_msg_to_qq(title=title,
+                                        text=text,
+                                        image=""if image == None else image,
+                                        user_id="Anjoy")
         except Exception as msg_e:
-            logger.error(f"PushDeer消息发送失败，错误信息：{str(msg_e)}")
+            logger.error(f"QQ消息发送失败，{str(msg_e)}")
+
+    
+    def send_msg_to_qq(self, title, text="", image="", user_id=""):
+        message_url = self._send_msg_url
+        if text:
+            conent = "%s\n%s" % (title, text.replace("\n\n", "\n"))
+        else:
+            conent = title
+        if not user_id:
+            user_id = "@Anjoy"
+        req_json = {
+            "user_id": user_id,
+            "title": title,
+            "image": image,
+            "text": conent
+        }
+        return self.__post_request(message_url, req_json)
+
+    def __post_request(self, message_url, req_json):
+        """
+        向qq发送请求
+        """
+        headers = {'content-type': 'application/json'}
+        try:
+            res = RequestUtils(headers=headers).post(message_url,
+                                                     data=json.dumps(req_json, ensure_ascii=False).encode('utf-8'))
+            if res and res.status_code == 200:
+                ret_json = res.json()
+                if ret_json.get('status') == 0:
+                    return True, ret_json.get('msg')
+                else:
+                    return False, ret_json.get('msg')
+            elif res is not None:
+                return False, f"错误码：{res.status_code}，错误原因：{res.reason}"
+            else:
+                return False, "未获取到返回信息"
+        except Exception as err:
+            return False, str(err)
+    
 
     def stop_service(self):
         """
